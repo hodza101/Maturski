b65bcf995cbc2a464d28b6cd9eed335f1d585041
Edin <hodzicedin078@gmail.com>
2026-05-03 18:16:54 +0200

    Add all project files

---
+++ testoviArduino/testoviArduino.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo servo1;
+
+void setup() {
+  servo1.attach(5);
+}
+
+void loop() {
+  servo1.write(0);
+  delay(1000);
+
+  servo1.write(90);
+  delay(1000);
+
+  servo1.write(180);
+  delay(1000);
+}
